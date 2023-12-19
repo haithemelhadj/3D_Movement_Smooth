736b271c2db382925c8c71129c27c9b54169ca50
Elhaj Elhaithem <haithemelhadj42@gmail.com>
2023-12-19 18:16:13 +0100

    commit

---
+++ Assets/LedgeHangScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LedgeHangScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /*
+     * to hang on ledge :
+     * when in air cast two rays :
+     * one that is above the player head 
+     * one that is below the player face 
+     * if the the above ray is hitting nothing and the below ray is hitting something
+     * then set the player to ledge hanging state
+     * 
+    */
+}
